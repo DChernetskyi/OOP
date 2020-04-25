f931a01f2ac31454c31b09811e7145b6eae748dc
Дмитрий Чернецкий <0639651043w@gmail.com>
2020-04-26 01:01:31 +0300

    класс, экземпляр

--- class/class/Program.cs
+++ class/class/Program.cs
@@ -11,12 +11,17 @@ namespace @class
         public string name;
         public string colour;
         public int start;
-        public string motor;
+        public string type;
+        public int kW;
+        public void Motor()
+        {
+            Console.WriteLine($"тип: {type}  кВт: {kW}");
+        }
         public string wheel;
         public int door;
         public void Info()
         {
-            Console.WriteLine($"модель: {name}  цвет: {colour}  количество стартов: {start} \n двигатель: {motor}  колесо: {wheel}  дверей: {door}");
+            Console.WriteLine($"модель: {name}  цвет: {colour}  количество стартов: {start} \n колесо: {wheel}  дверей: {door}");
         }
     }
     class Program
@@ -27,9 +32,11 @@ namespace @class
             Audi.start = 5;
             Audi.colour = "black";
             Audi.name = "A8";
-            Audi.motor = "diesel";
             Audi.wheel = "R19";
             Audi.door = 4;
+            Audi.type = "diesel";
+            Audi.kW = 175;
+            Audi.Motor();
             Audi.Info();
 
             Console.ReadLine();
