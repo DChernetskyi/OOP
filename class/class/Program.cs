5353afcec0526117543e0dd708d5212d4ca04434
Дмитрий Чернецкий <0639651043w@gmail.com>
2020-04-26 00:18:10 +0300

    класс,  экземпляр

--- class/class/Program.cs
+++ class/class/Program.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace @class
 {
+    class Car
+    {
+        public string name;
+        public string colour;
+        public int start;
+        public void Info()
+        {
+            Console.WriteLine($"модель: {name}  цвет: {colour}  количество стартов: {start}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Car Audi = new Car();
+            Audi.start = 5;
+            Audi.colour = "black";
+            Audi.name = "A8";
+            Audi.Info();
 
+            Console.ReadLine();
+
+            
         }
+        
     }
 }
